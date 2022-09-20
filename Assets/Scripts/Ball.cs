812f341ca836b86608703955bb6da88c3dafe1e0
HeathDoMay <112213670+HeathDoMay@users.noreply.github.com>
2022-09-19 18:39:05 -0600

    ZIP YOUR MOM
    pogger

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    public Collider coll;
+    public bool useGravity = false;
+    public Rigidbody rb;
+    public bool throwCheck = false;
+
+    public GameObject ball;
+    void Start()
+    {
+        coll = GetComponent<Collider>();
+        coll.isTrigger = true;
+        rb.useGravity = false;
+        throwCheck = false;
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Activate rigidbody and apply force on tap
+    void Update()
+    {
+        
+        if (Input.GetKeyDown("space") && throwCheck == false)
+        {
+            rb.useGravity = true;
+            rb.AddForce(0, 6, 6, ForceMode.Impulse);
+            throwCheck = true;
+        }
+    }
+
+    //cup detection
+    private void OnTriggerEnter(Collider other)
+    {  
+        if (other.tag == "Cup")
+        {
+            Destroy(other.gameObject);
+        }
+        Vector3 ballStart = new Vector3(0.02523589f, 0.0204134f, 1.0533f);
+        Instantiate(ball, ballStart, ball.transform.rotation);
+        Destroy(gameObject);
+    }
+}
