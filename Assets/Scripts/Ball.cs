a5a7a81026cc88595071c830905a3d19834744f4
AaronSmith148 <113400581+AaronSmith148@users.noreply.github.com>
2022-10-07 23:16:26 -0600

    Improved ball spawning
    Fixed spawn position issue and fixed throw direction

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -43,13 +43,8 @@ public class Ball : MonoBehaviour
             Destroy(other.gameObject);
         }
 
-        ballStart.x = parent.position.x;
-        ballStart.y = parent.position.y - 0.29f;
-        ballStart.z = parent.position.z + 0.73f;
+        ballStart = parent.position + (parent.forward*0.75f) - (parent.up * 0.25f);
         Instantiate(ball, ballStart, parent.rotation);
-        
-        
-
         Destroy(gameObject);
     }
 
@@ -57,7 +52,8 @@ public class Ball : MonoBehaviour
     public void ThrowBall()
     {
         rb.useGravity = true;
-        rb.AddForce(0, 6, 6, ForceMode.Impulse);
+        rb.AddRelativeForce(Vector3.forward * 5, ForceMode.Impulse);
+        rb.AddForce(Vector3.up * 5, ForceMode.Impulse);
         throwCheck = true;
     }
 }
